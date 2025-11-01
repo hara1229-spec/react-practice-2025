bfe0533cb24284877682f5f8992cd40a4fa3683e
T Hara <hara1229@gmail.com>
2025-11-01 11:03:02 +0900

    MVP: basic task add/remove/toggle with localStorage

--- react-practice1/src/App.jsx
+++ react-practice1/src/App.jsx
@@ -1,6 +1,14 @@
 import React from "react";
 import Home from "./pages/Home";
+import Header from "./components/Header";
 
-export default function App(){
-  return <div className="app"><Home /></div>;
+export default function App() {
+  return (
+    <div className="app">
+      <Header />
+      <main style={{ padding: "20px" }}>
+        <Home />
+      </main>
+    </div>
+  );
 }
---
+++ react-practice1/src/components/Header.jsx
@@ -0,0 +1,7 @@
+export default function Header() {
+  return (
+    <header style={{ background: "#333", color: "white", padding: "10px" }}>
+      <h1>TODOアプリ</h1>
+    </header>
+  );
+}
